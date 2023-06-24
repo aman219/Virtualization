61be715152293fdcdad6b0da530d5db40d811ba0
aman219 <65351629+aman219@users.noreply.github.com>
2023-06-24 23:19:35 +0530

    Hello triangle on window

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -1,7 +1,40 @@
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 #include "../../Engine/include/engine.h"
 
 int main()
 {
+	GLFWwindow* window;
+	if (!glfwInit())
+		return -1;
+	window = glfwCreateWindow(640, 480, "Virtualization", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "unable to create window\n";
+		glfwTerminate();
+	}
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+	std::cout << "Version : " << glGetString(GL_VERSION);
 	Engine e;
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.1f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
--- Engine/src/engine.cpp
+++ Engine/src/engine.cpp
@@ -2,10 +2,27 @@
 
 Engine::Engine()
 {
-	std::cout << "This is engine\n";
+	std::cout << "\nThis is engine\n";
+
+	float vertex[] = {
+			0.0f, 0.5f, 0.0f,
+			-0.5f, -0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f
+		};
+
+	unsigned int v;
+
+	glGenBuffers(1, &v);
+	glBindBuffer(GL_ARRAY_BUFFER, v);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex), vertex, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
 }
 
 Engine::~Engine()
 {
-	std::cout << "This is engine destructor\n";
+	std::cout << "\nThis is engine destructor\n";
 }
