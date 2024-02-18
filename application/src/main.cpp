b321928dc7df7c0f97d1e4da8d660b47319090e6
Aman Patel <ap6262997@gmail.com>
2024-02-18 16:08:47 +0530

    add external module libraries and modules

---
+++ application/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "This is application module\n";
+    return 0;
+}
---
+++ engine/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "This is engine module\n";
+    return 0;
+}
